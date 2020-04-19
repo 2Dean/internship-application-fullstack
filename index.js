aead88e2feee2cb28e4ed112adbe8c0613919e94
2Dean <42788697+2Dean@users.noreply.github.com>
2020-04-18 22:13:46 -0700

    Updated index.js to fetch urls, and display them
    Update index.js to meet basic project requirements, fetching an array of urls, returning 1 url at random, and displaying the returned url.

--- index.js
+++ index.js
@@ -1,12 +1,40 @@
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
+
 /**
- * Respond with hello worker text
+ * Respond to the request
  * @param {Request} request
  */
+
+//this function will fetch a json file from the given url, and
+//then return a second url from it, and display it
 async function handleRequest(request) {
-  return new Response('Hello worker!', {
-    headers: { 'content-type': 'text/plain' },
-  })
+  //variable decleration
+  //initial fetch request to the given json file
+   var urlRequested, fetch1, response, jsonTable;
+   //secondary fetch based on random number generation
+   var urlDerived, fetch2, response2;
+
+   urlRequested = `https://cfw-takehome.developers.workers.dev/api/variants`;
+   fetch1 = fetch(urlRequested);
+   response = await fetch1;
+   jsonTable = await response.json();    
+
+  //randomizer 0 - 100
+  var j = Math.round(Math.random()*100);    
+  
+  
+  //determine what variant to be sent to with 50/50 probability
+  j = j % 2;
+  if(j){
+      URL2 = jsonTable.variants[0];
+  }else{
+      URL2 = jsonTable.variants[1];
+  }
+   
+   //fetch the variant and display it
+   fetch2 = fetch(URL2)
+   response2 = await fetch2;
+   return response2;
 }
